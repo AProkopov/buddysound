66e7e6b37494a31e474e5271ba9341da171afe5d
LongLakesIPT <prokopov.anton@gmail.com>
2016-09-14 22:36:26 +0300

    Added sound files for dislikeButton. Added code to run onDislikeButtoClicked

--- app/src/main/java/com/example/android/buddysound/ButtonActivity.java
+++ app/src/main/java/com/example/android/buddysound/ButtonActivity.java
@@ -17,7 +17,8 @@ public class ButtonActivity extends AppCompatActivity {
     //необходимо в связи с проигрыванием файлов по порядку и произвольно
     public int likeSound = 0;
     public int dislikeSound = 0;
-    MediaPlayer mp;
+    MediaPlayer mpLike;
+    MediaPlayer mpDislike;
     Random random = new Random();
 
 
@@ -30,58 +31,125 @@ public class ButtonActivity extends AppCompatActivity {
         ((RadioButton)findViewById(R.id.orderRadio)).setChecked(true);
     }
 
+    //вызывается при клике на кнопке (dislikeButton)
     public void onDislikeClick (View view) {
 
+        Log.w("Before onLikeClick", "likeSound = " + dislikeSound);
+
+        if (((RadioButton)findViewById(R.id.orderRadio)).isChecked()) {
+            if(dislikeSound >= 8) {
+                dislikeSound = 1;
+            }
+            else dislikeSound++;
+
+            Log.w("Before play()", "likeSound = " + dislikeSound);
+            playLike(dislikeSound);
+        }
+        else {
+            dislikeSound = random.nextInt(7) + 1;
+            Log.w("Before play()", "likeSound = " + dislikeSound);
+            playLike(dislikeSound);
+        }
 
     }
 
     //вызывается при клике на кнопке (likeButton)
     public void onLikeClick (View view) {
-        //mp.stop();
+
         Log.w("Before onLikeClick", "likeSound = " + likeSound);
 
         if (((RadioButton)findViewById(R.id.orderRadio)).isChecked()) {
-            if(likeSound >= 4) {
+            if(likeSound >= 8) {
                 likeSound = 1;
             }
             else likeSound++;
 
             Log.w("Before play()", "likeSound = " + likeSound);
-            play(likeSound);
+            playDisLike(likeSound);
         }
         else {
-            likeSound = random.nextInt(3) + 1;
+            likeSound = random.nextInt(7) + 1;
             Log.w("Before play()", "likeSound = " + likeSound);
-            play(likeSound);
+            playDisLike(likeSound);
         }
     }
 
     //метод проигрывает звуковой файл в зависимости от порядкового номера файла,
     //опеределенного при отработке метода onLikeClick
-    public void play(int x) {
+    public void playLike(int x) {
 
-        if (mp != null) {
-            mp.stop();
+        if (mpLike != null) {
+            mpLike.stop();
         }
 
         switch (x) {
             case 1:
-                mp = MediaPlayer.create(this, R.raw.laugh_w);
-                mp.start();
+                mpLike = MediaPlayer.create(this, R.raw.applause);
+                mpLike.start();
                 break;
             case 2:
-                mp = MediaPlayer.create(this, R.raw.benny);
-                mp.start();
+                mpLike = MediaPlayer.create(this, R.raw.benny);
+                mpLike.start();
                 break;
             case 3:
-                mp = MediaPlayer.create(this, R.raw.radio_maximum);
-                mp.start();
+                mpLike = MediaPlayer.create(this, R.raw.burunduk);
+                mpLike.start();
                 break;
             case 4:
-                mp = MediaPlayer.create(this, R.raw.applo);
-                mp.start();
+                mpLike = MediaPlayer.create(this, R.raw.chgk);
+                mpLike.start();
+            case 5:
+                mpLike = MediaPlayer.create(this, R.raw.eralash);
+                mpLike.start();
+            case 6:
+                mpLike = MediaPlayer.create(this, R.raw.laugh);
+                mpLike.start();
+            case 7:
+                mpLike = MediaPlayer.create(this, R.raw.radiomax);
+                mpLike.start();
+            case 8:
+                mpLike = MediaPlayer.create(this, R.raw.shurik);
+                mpLike.start();
         }
     }
 
+    //метод проигрывает звуковой файл в зависимости от порядкового номера файла,
+    //опеределенного при отработке метода onDislikeClick
+    public void playDisLike(int x) {
+
+        if (mpDislike != null) {
+            mpDislike.stop();
+        }
+
+        switch (x) {
+            case 1:
+                mpDislike = MediaPlayer.create(this, R.raw.cartoon);
+                mpDislike.start();
+                break;
+            case 2:
+                mpDislike = MediaPlayer.create(this, R.raw.pooo_1);
+                mpDislike.start();
+                break;
+            case 3:
+                mpDislike = MediaPlayer.create(this, R.raw.pooo_2);
+                mpDislike.start();
+                break;
+            case 4:
+                mpDislike = MediaPlayer.create(this, R.raw.pooo_3);
+                mpDislike.start();
+            case 5:
+                mpDislike = MediaPlayer.create(this, R.raw.proval);
+                mpDislike.start();
+            case 6:
+                mpDislike = MediaPlayer.create(this, R.raw.riot);
+                mpDislike.start();
+            case 7:
+                mpDislike = MediaPlayer.create(this, R.raw.toilet);
+                mpDislike.start();
+            case 8:
+                mpDislike = MediaPlayer.create(this, R.raw.trombon);
+                mpDislike.start();
+        }
+    }
 
 }
